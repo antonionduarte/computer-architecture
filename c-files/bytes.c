20372664f84f8c689389e448b78969e88507b812
António Duarte <antonioduarte@Antonio-Duarte.lan>
2020-03-09 20:06:28 +0000

    Changes to bytes.c

--- c-files/bytes.c
+++ c-files/bytes.c
@@ -9,7 +9,12 @@ int main() {
 	printf("Integer Bytes: %lu\n", sizeof(int));
 	printf("Integer Max Value: %d\n", INT_MAX);
 	printf("Integer Min Value: %d\n", INT_MIN);
-	printf("Unsigned Integer Bytes: %lu, Range: %u .. %d", sizeof(unsigned int), UINT_MAX, 0);
+	printf("Unsigned Integer Bytes: %lu, Range: %u .. %d\n", sizeof(unsigned int), UINT_MAX, 0);
 	// Note the usage of %lu for unsigned longs, %u for unsigned ints, %d for ints.
+	printf("\n");
+	printf("Long Bytes: %lu\n", sizeof(long));
+	printf("Long Max Value: %ld\n", LONG_MAX);
+	printf("Long Min Value: %ld\n", LONG_MIN);
+	printf("Unsigned Long Bytes: %lu, Range: %lu .. %d\n", sizeof(unsigned long), ULONG_MAX, 0);
 	printf("\n");
 }
