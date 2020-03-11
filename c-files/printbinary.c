abb6f440641142a57e49aee4543fa9e19f298d5a
António Duarte <antonionpnduarte@gmail.com>
2020-03-11 23:21:59 +0000

    Finished printbinary.c

--- c-files/printbinary.c
+++ c-files/printbinary.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
 
-void printbin(int val);
+// Constants
+
+#define MAX_SIZE 9
+
+// Declarations
+
+void printBinary(int val);
+
+// Main
 
 int main() {
+    for (int i = 256; i > 0; i--)
+        printBinary(i);
+
 
 }
 
-void printBin(int val) {
-    for (int i = 0; )
+// Functions
+
+void printBinary(int val) {
+    int result[MAX_SIZE];
+
+    for (int i = 0; i <= MAX_SIZE - 1; i++)
+        result[i] = 0;
+
+    for (int i = 0; val >= 1 && i <= MAX_SIZE - 1; i++) {
+        result[i] = val % 2;
+        val = val / 2;
+    }
+
+    for (int i = MAX_SIZE - 1; i >= 0; i--) {
+        int binary = result[i];
+        printf("%d", binary);
+    }
+
+    printf("\n");
 }
 
