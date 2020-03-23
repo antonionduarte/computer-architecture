e17bfaa696dc691b7392a61ccefdbbbbe04fabac
António Duarte <antonionpnduarte@gmail.com>
2020-03-23 17:01:08 +0000

    Added new files

---
+++ computer-architecture/study1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Tasks para hoje:
+    - (2) Refazer o programa de ler binário mas com bitwise operators.
+    - (1) Fazer os exercícios da ficha 1 todos.
+    - (0) Estudar floating point conversion.
+*/
+
+int main() {
+    
+    return 0;
+}
---
+++ java/StringParser.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class StringParser {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(parseRoom(scanner.nextLine()));
+        scanner.close();
+    }
+
+    private static String parseRoom(String toParse) {
+        StringBuilder builder = new StringBuilder();
+        int finalIndexOfRoom = -1;
+        int finalIndexOfEd = -1;
+
+        builder.append("(");
+
+        if (toParse.contains("Ed"))
+            finalIndexOfRoom = toParse.indexOf("Ed");
+        else if (toParse.contains("Lab"))
+            finalIndexOfRoom = toParse.indexOf("Lab");
+
+        if (finalIndexOfRoom == -1)
+            return "'None'";
+
+        else {
+            builder.append("'");
+            for (int i = 1; i < finalIndexOfRoom; i++) {
+                if (i == 1) builder.append(toParse.charAt(0)); else builder.append(toParse.charAt(i - 1));
+            }
+            builder.append("'"); builder.append(", ");
+        }
+
+        builder.append(")");
+        return builder.toString();
+    }
+}
