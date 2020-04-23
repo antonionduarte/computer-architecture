ee5c548123a2c9730d2905ecd001602b7f5da746
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-23 19:46:51 +0100

    Finished sum-two-by-two

--- assembly/sum-two-by-two.s
+++ assembly/sum-two-by-two.s
@@ -4,8 +4,31 @@ LINUX_SYSCALL = 0x80
 
 .data
 vetor: .int 1, 2, 5, 7 ,8, 5 # um vetor de inteiros
-len = (. - vetor) / 4
+LEN = (. - vetor) / 4 # comprimento do vetor (notar que está a maíscula pois é uma constante)
 
-# explicando a len, basicamente vetor é a primeira posição do vetor, e len é a última (o ponto corrente)
-# fazendo a sua subtração obtemos o tamanho em Bytes dos vetor, para obtermos a length do vetor
+resultado: .int 0 # variável onde é guardado o comprimento do vetor
+
+# explicando a constante LEN, basicamente "vetor" é a primeira posição do vetor, e "." é a última (o ponto corrente)
+# fazendo a sua subtração ". - vetor" obtemos o tamanho em Bytes do vetor, logo para obtermos o comprimento do vetor
 # realizamos a sua divisão por quatro (pois cada inteiro ocupada 4 bytes)
+
+.text # secção de código
+.global _start
+_start:
+    mov $vetor, %eax # movemos o vetor em si (endereço de memória) e não o conteúdo do vetor
+    mov (%eax), %ebx # agora movemos o conteúdo de %eax usando "()" para ebx
+                     # esse conteúdo será o valor do primeiro elemento do vetor (%ebx é onde guardamos a soma)
+    mov $LEN, %ecx # guardar o LEN (comprimento do vetor) no registo %ecx 
+ciclo:
+    dec %ecx
+    jz fim
+    add $4, %eax # aumentar em 4 bytes ebx, indo portanto para o próximo inteiro
+    add (%eax), %ebx # somar o conteúdo do próximo elemento do vetor a %ebx
+    jmp ciclo # saltar de novo para o inicio do ciclo
+fim:
+    mov  %ebx, soma # guardar o resultado na variável "soma"
+
+    mov $0, %ebx
+    mov $EXIT, %eax
+    int $LINUX_SYSCALL, %eax
+
