035d7a91904a45886bf6d5fa050a43446ea293d4
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-08 21:05:39 +0100

    New files

---
+++ assembly/match.s
@@ -0,0 +1,12 @@
+#
+#  MIEI 19/20 FCT/UNL
+#
+
+
+########
+# int match( int a, int b, int c )
+#
+.text
+.globl	match
+
+
---
+++ assembly/mycompare.s
@@ -0,0 +1,12 @@
+#
+#  MIEI 19/20 FCT/UNL
+#
+
+
+########
+# int mycompare( int size, int x[], int y[] )
+#
+.text
+.globl	mycompare
+
+
