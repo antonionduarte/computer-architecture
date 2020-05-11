e9e73275e6aa8e9096817fc38917fafdc294ad61
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-11 21:05:34 +0100

    Finished match and mycompare

--- assembly/match.s
+++ assembly/match.s
@@ -8,5 +8,31 @@
 #
 .text
 .globl	match
+match:
+    push %ebp
+    mov %esp, %ebp
+    mov 8(%ebp), %eax # move the variable "a" to %eax
+    mov 12(%ebp), %ecx # move the variable "b" to %ecx
+    mov 16(%ebp), %edx # move the variable "c" to %edx
 
+    test %eax, %ecx # (a & b) != 0
+    jz wrong
 
+    test %eax, %edx # (a & c) == 0
+    jnz wrong
+
+    test %ecx, %edx # (b & c) == 0
+    jnz wrong
+
+    jmp right
+
+wrong:
+    movl $0, %eax # move 0 to %eax, so the function returns false
+    jmp end
+
+right:
+    movl $1, %eax # move 1 to %eax so the function returns true
+
+end:
+    pop %ebp # return %ebp to the original state
+    ret
--- assembly/mycompare.s
+++ assembly/mycompare.s
@@ -8,5 +8,38 @@
 #
 .text
 .globl	mycompare
+mycompare:
+    push %ebp
+    mov %esp, %ebp
 
+    mov 16(%ebp), %eax # move memory position of y[0] to %eax
+    mov 12(%ebp), %ecx # move memory position of x[0] to %ecx
+    movl $0, %edx # move 0 to %edx, this register will act as a counter
 
+    push %ebx # save the current content of %ebx
+
+cicle:
+    cmp 8(%ebp), %edx
+    jz equal
+
+    mov (%eax), %ebx # move y[0] to %ebx
+    cmp %ebx, (%ecx,%edx,4) # compares y[0] with x[0]
+    jnz not_equal
+
+    add $4, %eax # sum 4 bits to increment current position of y[]
+    inc %edx # increments the counter
+
+    jmp cicle
+
+not_equal:
+    mov %edx, %eax # move counter to %eax so the function returns current position
+    jmp end
+
+equal:
+    movl $-1, %eax # move -1 to %eax so the function returns (-1)
+    jmp end
+
+end: 
+    pop %ebx # return content of %ebp to the original state
+    pop %ebp # return content of %ebx to the original state
+    ret
