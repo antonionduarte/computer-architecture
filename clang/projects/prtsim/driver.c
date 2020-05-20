309b438002a6df30e10295f46909445c41104f0e
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-05-20 23:20:49 +0100

    Made pseudo-constants

--- clang/projects/prtsim/driver.c
+++ clang/projects/prtsim/driver.c
@@ -8,23 +8,27 @@
 #include "hw.h"
 #include "driver.h"
 
+#define BYTE_TX 0x100
+#define COMMAND 0x101
+#define STATUS 0x102
+
 int sendByte( unsigned char c ) {
 
-	out (0x100, c); // places the byte to print in the BYTE_TX register (0x100)
+	out (BYTE_TX, c); // places the byte to print in the BYTE_TX register (0x100)
 	unsigned char status; // status of the printer
 
 	/* Send the byte to print when the printer has the correct STATUS */
 
 	do {
-		status = in(0x102);
+		status = in(STATUS);
 	} while (status != 0b100);
 	
-	out (0x101, 1); // place the correct command on the COMMAND register (0x101)
+	out (COMMAND, 1); // place the correct command on the COMMAND register (0x101)
 
 	/* Waits until the printer has the correct value (0b100) in the STATUS register (0x102) and is ready for the next Byte */
 
 	do {
-		status = in(0x102); // gets the correct status from STATUS register (0x102)
+		status = in(STATUS); // gets the correct status from STATUS register (0x102)
 		if ( (status & 0b100) == 0 ) return 0; // If the printer is offline, returns 0 
 	} while (status != 0b100); // repeats it until the printer is in the correct state to print the byte
 
